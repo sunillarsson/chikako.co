bd6f6ebbce4e59e1798fe021f3ab551b5c7e9ab3
sunillarsson <35098176+sunillarsson@users.noreply.github.com>
2025-03-23 21:57:19 +0100

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
-app.set('layout', 'views/layout');
+app.set('layout', 'layout');
 
 // Routes
 app.get('/', (req, res) => {
